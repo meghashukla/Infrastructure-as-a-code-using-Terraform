064584267f15b8e4201f4dc1a4f5d1928f4b8cf2
Megha Shukla <msmeghashukla@gmail.com>
2021-03-08 05:02:34 +0530

    made changes to subnet azs and subnet ids

--- modules/asg/variable.tf
+++ modules/asg/variable.tf
@@ -3,12 +3,13 @@ variable instance_id{}
 variable instance_type{}
 variable security_groups{}
 variable key_name{}
-variable subnetazs{
-    type = list
-}
+#variable subnetazs{
+#    type = list
+#}
 variable min_size{}
 variable max_size{}
 variable desired_capacity{}
 variable load_balancer_name{}
 variable target_group_arn{}
+variable subnet_ids{}
 
