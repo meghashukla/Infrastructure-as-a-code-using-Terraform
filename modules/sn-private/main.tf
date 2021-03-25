0925b456d6da09a42b5644cc39ceea5cd6ca5624
Megha Shukla <msmeghashukla@gmail.com>
2021-03-25 21:43:30 +0530

    Corrected the NAT gateway

--- modules/sn-private/main.tf
+++ modules/sn-private/main.tf
@@ -14,7 +14,7 @@ resource "aws_eip" "eip" {
 resource "aws_nat_gateway" "masterngw" {
 
   allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.private.id
+  subnet_id     = var.publicsubnet
 
   tags = {
     Name = "ngw-${var.subnet_name}"
@@ -32,7 +32,7 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route" "nat_gateway_route" {
   route_table_id         = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.masterngw.id
+  nat_gateway_id             = aws_nat_gateway.masterngw.id
 }
 
 resource "aws_route_table_association" "rt_assn" {
